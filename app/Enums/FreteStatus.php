33448fc5ca001f8bc29d5698f19de50c8ce89420
Felipe Santos <felipesantoss427@gmail.com>
2025-05-01 14:33:32 -0300

    Adição máscara ao campo de telefone

--- app/Enums/FreteStatus.php
+++ app/Enums/FreteStatus.php
@@ -4,14 +4,14 @@ namespace App\Enums;
 
 enum FreteStatus: string
 {
-    case Em_TRANSITO = 'Em Trânsito';
+    case EM_TRANSITO = 'Em Trânsito';
     case SAIU_PARA_ENTREGA = 'Saiu para entrega';
     case ENTREGUE = 'Entregue';
 
     public function corEtiqueta(): string 
     {
         return match($this) {
-            self::Em_TRANSITO => 'bg-yellow-100 text-yellow-800',
+            self::EM_TRANSITO => 'bg-yellow-100 text-yellow-800',
             self::SAIU_PARA_ENTREGA => 'bg-blue-100 text-blue-800',
             self::ENTREGUE => 'bg-green-100 text-green-800',
         };
